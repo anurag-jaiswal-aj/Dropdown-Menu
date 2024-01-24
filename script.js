a3ce08fc8faac61c36f0c47bf109d4791ac61150
Anurag Jaiswal <janurag582004@gmail.com>
2024-01-24 12:49:54 +0530

    .

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ var options = document.getElementsByClassName("options");
 var list = document.getElementById("list");
 var arrowIcon = document.getElementById("arrowIcon");
 
+
 selectField.onclick = function () {
     list.classList.toggle("hide");
     arrowIcon.classList.toggle("rotate");
 }
+
 
 for (option of options) {
     option.onclick = function () {
